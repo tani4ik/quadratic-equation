73dc59b248454c14935a953760b9b4439d406582
Tatsiana Sakalinskaya <tani4ik@gmail.com>
2018-09-26 23:30:47 +0300

    Implemented quadratic equations

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let regexp = /(-?\d+) \* x\^2 ([+-] \d+) \* x ([+-] \d+)/;
+  let values = equation.match(regexp);
+  let a = values[1];
+  let b = values[2].replace(/ /, '');
+  let c = values[3].replace(/ /, '');
+  let D = b * b - 4 * a * c;
+  let x1 = (- b + Math.round(Math.sqrt(D))) / 2 / a;
+  let x2 = (- b - Math.round(Math.sqrt(D))) / 2 / a;
+
+  return [x1, x2].sort((x1, x2) => x1 - x2);
 }
